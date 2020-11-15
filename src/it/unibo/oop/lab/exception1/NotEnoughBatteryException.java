b74c864c75df8e0326698047d0da2e4458b264fb
Matteo <matteo.violani@studio.unibo.it>
2020-11-15 21:22:48 +0100

    add comment to explain exception type

--- src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
+++ src/it/unibo/oop/lab/exception1/NotEnoughBatteryException.java
@@ -1,7 +1,11 @@
 package it.unibo.oop.lab.exception1;
 
 public class NotEnoughBatteryException extends IllegalStateException{
-
+    /*
+     * I decided to extends IllegalStateException because is more expressive, javadoc says "Java environment 
+     * or Java application is not in an appropriate state for the requested operation." In this case 
+     * robot enviroment is not in a state that allow the robot to move
+     * */
     /**
      * 
      */
